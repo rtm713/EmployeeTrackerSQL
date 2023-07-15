a6d604e47bc13fea768b873eee275efe221eca9e
rtm713 <rtm713@gmail.com>
2023-07-15 17:30:39 -0500

    joined tables and got them to render what i want

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -20,5 +22,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
     manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,8 @@ USE employee_db;
 INSERT INTO departments (name) 
     VALUES ('ENGINEERING'), ('SALES'),("FINANCE"),("LEGAL");
 
-INSERT INTO roles (title, salary)
-    VALUES ('SALES MANAGER', '60000'),('SALESPERSON', '40000');
+INSERT INTO roles (title, salary, department_id)
+    VALUES ('SALES MANAGER', 60000, 2),('SALESPERSON', 40000, 2);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES ('John', 'Smith', 1, NULL),('Jane', 'Miller', 2, 1);
