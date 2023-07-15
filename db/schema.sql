04ad2aa612834b4b318c813b377715f5834ae6ef
rtm713 <rtm713@gmail.com>
2023-07-15 02:43:35 -0500

    added roles and employee tables and updated seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,4 +6,19 @@ USE employee_db;
 CREATE TABLE departments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,6 @@ USE employee_db;
 
 INSERT INTO departments (name) 
     VALUES ('ENGINEERING'), ('SALES'),("FINANCE"),("LEGAL");
+
+INSERT INTO roles (title, salary)
+    VALUES ('SALES MANAGER', '60000'),('SALESPERSON', '40000');
