00400269077a3c138cecceb3d61948f163800e94
rtm713 <rtm713@gmail.com>
2023-07-18 02:28:36 -0500

    added department deletion functionality

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE roles (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -23,8 +24,10 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id),
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
     manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,14 @@
 USE employee_db;
 
 INSERT INTO departments (name) 
-    VALUES ('ENGINEERING'), ('SALES'),("FINANCE"),("LEGAL");
+    VALUES ('ENGINEERING'), ('SALES'),("FINANCE");
 
 INSERT INTO roles (title, salary, department_id)
-    VALUES ('SALES MANAGER', 60000, 2),('SALESPERSON', 40000, 2);
+    VALUES ('SALES MANAGER', 85000, 2),('SALESPERSON', 50000, 2),
+    ('SENIOR ENGINEER', 90000, 1),('JUNIOR ENGINEER', 65000, 1),
+    ('FINANCE DIRECTOR', 75000, 3),('PAYROLL SPECIALIST', 45000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-    VALUES ('John', 'Smith', 1, NULL),('Jane', 'Miller', 2, 1);
+    VALUES ('John', 'Smith', 1, NULL),('Shelby', 'Miller', 2, 1),
+    ('Hunter', 'Johnson', 3, NULL),('Trey', 'Birmingham', 4, 3),
+    ('Brittany', 'Wilson', 5, NULL),('Michael', 'Spears', 6, 5);
